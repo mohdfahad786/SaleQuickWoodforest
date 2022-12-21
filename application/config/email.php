00ee17aac5f66da71627d4d263bc4ff9e0fd505d
shuaebahmad15 <31515593+shuaebahmad15@users.noreply.github.com>
2022-12-21 20:20:17 +0530

    reseller report

--- application/config/email.php
+++ application/config/email.php
@@ -9,13 +9,19 @@
 // $config['newline'] = "\r\n";
 
 $config['protocol'] = 'smtp';
-$config['smtp_host'] = 'ssl://in.mailjet.com';
+//$config['smtp_host'] = 'ssl://in.mailjet.com';
+$config['smtp_host'] = 'ssl://in-v3.mailjet.com';
 $config['smtp_port'] = '465';
 // $config['smtp_user'] = 'bd44f3f110259eb60f880abdc2de47e3';
 // $config['smtp_pass'] = '571258666c3347fbf47fbf12850f00e7';
 
 $config['smtp_user'] = 'bd44f3f110259eb60f880abdc2de47e3';
-$config['smtp_pass'] = 'ba3bfdc0084854f30c0a6da271d30428';
+//$config['smtp_pass'] = 'ba3bfdc0084854f30c0a6da271d30428';
+$config['smtp_pass'] = 'fa94fe58aaa9459034a81577fbc045c0';
+
+
+
+
 $config['charset'] = 'utf-8';
 $config['mailtype'] = 'html';
 $config['newline'] = "\r\n";
